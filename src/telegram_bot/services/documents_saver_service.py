400088c25d9c0c2e0be23a43c46a102f159732bb
Viacheslav <63041191+Viacheslav-hub-blip@users.noreply.github.com>
2025-03-22 08:27:45 +0300

    Update documents_saver_service.py
    Добавлены комментарии

--- src/telegram_bot/services/documents_saver_service.py
+++ src/telegram_bot/services/documents_saver_service.py
@@ -9,6 +9,7 @@ class DocumentsSaver:
         os.makedirs(f'/home/alex/PycharmProjects/pythonProject/src/users_directory/user_{user_id}', exist_ok=True)
 
     def save_source_docs_in_files(self, user_id: str, docs_id: List[str], documents: List[str]) -> None:
+        """Сохраняет документы в папку пользовател под их id"""
         self.__create_user_directory(user_id)
         for doc_id, document in zip(docs_id, documents):
             path = f'/home/alex/PycharmProjects/pythonProject/src/users_directory/user_{user_id}/{doc_id}.txt'
@@ -16,11 +17,13 @@ class DocumentsSaver:
                 file.write(document)
 
     def clear_user_directory(self, user_id: str) -> None:
+        """Удаляет папку с фрагментами документов"""
         self.__create_user_directory(user_id)
         shutil.rmtree(f'/home/alex/PycharmProjects/pythonProject/src/users_directory/user_{user_id}')
 
     @staticmethod
     def check_exist_user_directory(user_id: str) -> bool:
+        """Проверяет сущуствование папки пользователя с фрагментами документов"""
         if os.path.exists(f"/home/alex/PycharmProjects/pythonProject/src/users_directory/user_{user_id}"):
             return True
         return False
